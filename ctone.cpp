b9f670a5301adb2d999eb08916656ddf31078160
Anthony Nishiyama <datanishiyama@gmail.com>
2024-02-20 10:47:12 +0900

    Git Init and Final

---
+++ ctone.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//Struct Inputs
+struct Dwarf {
+    string firstName;
+    string lastName;
+    string streetAddress;
+    string city;
+    string zipCode;
+};
+
+int main() {
+    //New Dwarf Struct
+    Dwarf senshiDarkstout;
+    senshiDarkstout.firstName = "Senshi";
+    senshiDarkstout.lastName = "Darkstout";
+    senshiDarkstout.streetAddress = "Iron Hills";
+    senshiDarkstout.city = "321, Ironforge";
+    senshiDarkstout.zipCode = "3501154";
+
+    //Requested Output
+    cout << "CSC450: Module 1 Critical Thinking" << endl;
+    cout << "1. First Name: " << senshiDarkstout.firstName << endl;
+    cout << "2. Last Name: " << senshiDarkstout.lastName << endl;
+    cout << "3. Street Address: " << senshiDarkstout.streetAddress << endl;
+    cout << "4. City: " << senshiDarkstout.city << endl;
+    cout << "5. Zip Code: " << senshiDarkstout.zipCode << endl;
+
+    return 0;
+}
