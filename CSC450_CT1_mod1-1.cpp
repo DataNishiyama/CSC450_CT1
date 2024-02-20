e25c68b0415e90d4f6c4fd257dc64b74ccf2fb24
Anthony Nishiyama <datanishiyama@gmail.com>
2024-02-20 11:19:09 +0900

    Final Submission
    ctOne
    Mod1-1 Add and Corrections
    Mod1-2 Add and Corrections

---
+++ CSC450_CT1_mod1-1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+//Removed Import library function not used
+//#include<conio.h>
+
+//Standard namespace declaration
+using namespace std;
+
+//Main Function
+int main()
+{
+     //Standard Ouput Statement
+     cout << "Welcome to this C++ Program." << endl;
+     cout << "I have corrected all errors for this program." << endl;
+
+     //Main Function return Statement
+     return 0;
+
+     //End
+}
---
+++ CSC450_CT1_mod1-2.cpp
@@ -0,0 +1,31 @@
+
+
+#include<iostream>
+
+//Remove of <conio.h>
+
+//Standard namespace declaration
+using namespace std;
+
+//Main Function
+
+int main()
+{
+     //Corrected to Print Out
+     double myMoney = 1000.50;
+
+
+     //Standard Ouput Statement
+     cout <<"Please be sure to correct all syntax errors in this program." << endl;
+
+     cout <<"I have corrected all errors for this program." << endl;
+
+     cout <<"The total amount of money available is: = " << myMoney << endl;
+
+     //Wait For Output Screen
+
+     //Main Function
+
+     //Return Statement
+     return 0;
+}
